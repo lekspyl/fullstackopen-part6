b231815ed7d12980e6ded4a1fe58936a584b173a
Oleksandr Pylypenko <lex.pilipenko@gmail.com>
2022-10-27 20:26:20 +0300

    ex 6.8

---
+++ redux-anecdotes/src/components/AnecdoteForm.js
@@ -0,0 +1,24 @@
+import { useDispatch } from 'react-redux'
+import { createAnecdote } from '../reducers/anecdoteReducer'
+
+const AnecdoteForm = () => {
+  const dispatch = useDispatch()
+
+  const addAnecdote = (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    event.target.anecdote.value = ''
+    dispatch(createAnecdote(content))
+  }
+  return (
+    <div>
+      <h2>Add a new anecdote</h2>
+      <form onSubmit={addAnecdote}>
+        <div><input name="anecdote" /></div>
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  )
+}
+
+export default AnecdoteForm
